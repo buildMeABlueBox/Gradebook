a915dbcbee12c2c861c01b8b369d3313297a68b0
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2017-12-09 17:21:08 -0500

    * Gradebook.sln: sln file
    * Gradebook.userprefs: userprefs file
    
    * Gradebook.csproj: project file
    
    * AssemblyInfo.cs: assembly info

---
+++ Gradebook/Gradebook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    public class Gradebook
+    {
+        List<float> grades;
+        Statistics s;
+        public Gradebook()
+        {
+            grades = new List<float>();
+        }
+
+        public AddToGrades(float x){
+            grades.Add(x);
+        }
+    }
+}
+    
---
+++ Gradebook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Gradebook gb1 = new Gradebook();
+            gb1.AddToGrades(91);
+            gb1.AddToGrades(28.2f);
+
+            Gradebook gb2 = gb1;
+            gb2.AddToGrades(29);
+            gb1.AddToGrades(23);
+
+        }
+    }
+}
